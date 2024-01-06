a8b3213252d773caff1562a4f5bd9949b8d1a2cf
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-06 18:47:44 +0700

    Buổi 3: validation register form

--- event.js
+++ event.js
@@ -1,38 +1,36 @@
-// console.log("Buổi 2");
-// var newElement = document.createElement("div");
-// newElement.innerText = "Xin chao ca lop!";
-// document.body.appendChild(newElement);
-// document.body.removeChild(newElement);
-
-// function login() {
-//   console.log("Dang nhap thanh cong!");
-// }
-
-// // Cach 2:
-// var loginSubmit = document.getElementsByClassName("login")[1];
-// console.log({ loginSubmit });
-// loginSubmit.onclick = login;
-
-// // Cach 3:
-// var loginSubmit3 = document.getElementById("dangNhap");
-// loginSubmit3.addEventListener("click", login);
-
+var users = [];
+function validationForm(username, password, confirmPass) {
+  // Kiem tra du lieu
+  if (username === "" || password === "" || confirmPass === "") {
+    alert("khong duoc de trong!");
+    return false;
+  }
+  if (password.length < 6) {
+    alert("Password phai dai it nhat 6 ky tu!");
+    return false;
+  }
+  if (password !== confirmPass) {
+    alert("Confirm password không khớp!");
+    return false;
+  }
+  return true;
+}
 function register() {
   // Buoc 1: lay gia tri
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   var confirmPass = document.getElementById("confirmPass").value;
-  // Buoc 2: Dua vao 1 object
-  var userInfor = {
-    username,
-    password,
-    confirmPass,
-  };
 
-  // BOM = Browser Object Model
-  // Buoc 3: dua userInfor vao localStorage
+  if (validationForm(username, password, confirmPass)) {
+    // Buoc 2: Dua vao 1 object
+    var userInfor = {
+      username,
+      password,
+      confirmPass,
+    };
 
-  localStorage.setItem("userInfor", JSON.stringify(userInfor));
-
-  //JSON = Javascript Object Notation
+    users.push(userInfor);
+    // Buoc 3: dua userInfor vao localStorage
+    localStorage.setItem("users", JSON.stringify(users));
+  }
 }
