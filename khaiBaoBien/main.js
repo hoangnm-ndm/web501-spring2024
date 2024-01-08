e62833df57b73be77281e43ac2246a86cf0d9a60
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-08 16:30:53 +0700

    Buổi 3: update

---
+++ khaiBaoBien/main.js
@@ -0,0 +1,41 @@
+var a;
+console.log(a);
+{
+  var a = "hoang";
+  var a = "tiến bịp";
+  a = "ngo ba kha";
+  console.log(a);
+}
+// var a; a ="hoang"
+
+/**
+ * var = variant
+ * 1. Hoisting = Hành vi khai báo được đưa lên đầu scope.
+ * 2. Global scope = biến khai báo bằng var có phạm vi toàn cục.
+ * 3. var có thể khai báo lại nhiều lần
+ * 4. Khai báo với var có thể assign được.
+ */
+
+function sum(a, b) {
+  let b = 10;
+  let a = 20;
+  return a + b;
+}
+console.log(b);
+let myName = "hoang";
+// let myName = "Nguyen"
+/**
+ * let
+ * 1. let không hoisting
+ * 2. khai báo biến bằng let thì có phạm vi hoạt động là block scope và function scope
+ * 3. Không thể khai báo lại
+ * 4. Được phép gán lại.
+ */
+
+/**
+ * const = constant
+ * 1. const không hoisting
+ * 2. khai báo biến bằng const thì có phạm vi hoạt động là block scope và function scope
+ * 3. Không thể khai báo lại
+ * 4. Const không gán lại được.
+ */
--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function validationForm(username, password) {
   // Kiem tra du lieu
-  if (username === "" || password === "") {
+  if (!username || !password) {
     alert("khong duoc de trong!");
     return false;
   }
@@ -16,15 +16,10 @@ function login() {
   var password = document.getElementById("password").value;
 
   if (validationForm(username, password)) {
-    // Buoc 2: Dua vao 1 object
-    var userInfor = {
-      username,
-      password,
-    };
-
-    // Buoc 3: Lấy dữ liệu từ API (localStorage)
+    // Buoc 2: Lấy dữ liệu từ API (localStorage)
     if (localStorage.getItem("users")) {
       var dataLocalStorage = JSON.parse(localStorage.getItem("users"));
+      // Buoc 3: So sanh gia trị nhập vào và giá trị lấy từ localStorage
       dataLocalStorage.some((item) => {
         if (item.username === username && item.password === password) {
           alert("Dang nhap thanh cong!");
@@ -34,3 +29,16 @@ function login() {
     }
   }
 }
+
+/**
+ * trusthy và falsy
+ * 6 giá trị sau khi convert sang boolean sẽ là falsy:
+ * 1. false
+ * 2. null
+ * 3. undefined
+ * 4. 0
+ * 5. ""
+ * 6. NaN
+ */
+
+console.log(!!0);
