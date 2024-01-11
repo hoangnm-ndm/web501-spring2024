428457c47fd883599fef6aa12313f9275d59eb05
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-11 19:40:25 +0700

    Buổi 4: List products

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import "./style.css";
-import { AboutPage } from "./src/pages/AboutPage";
 import { HomePage } from "./src/pages/HomePage";
+import "./node_modules/bootstrap/dist/js/bootstrap";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 const app = document.getElementById("app");
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,21 @@
 export const HomePage = () => {
-  const datas = [
-    { id: "1", name: "Iphone 15", price: 2000, desc: "San pham moi" },
-    { id: "2", name: "Iphone 16", price: 3000, desc: "San pham moi" },
-    { id: "3", name: "Iphone 100", price: 1000000, desc: "San pham moi" },
-  ];
-  return `<h2>HomePage</h2>`;
+  return `<div>${html}</div>`;
 };
+
+export const datas = [
+  { id: "1", name: "Iphone 15", price: 2000, desc: "San pham moi" },
+  { id: "2", name: "Iphone 16", price: 3000, desc: "San pham moi" },
+  { id: "3", name: "Iphone 100", price: 1000000, desc: "San pham moi" },
+];
+
+const html = datas
+  .map((item) => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.price}</p>
+      <a href="#" class="btn btn-primary">Add to card</a>
+    </div>
+  </div>`;
+  })
+  .join("");
