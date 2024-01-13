2061d934b56e07f63293c236bf3cdac2ff61c64e
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-13 16:00:29 +0700

    add register form

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 import "./style.css";
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./node_modules/bootstrap/dist/js/bootstrap.js";
+import "./node_modules/bootstrap/dist/js/bootstrap";
 import Navigo from "navigo";
 import { render } from "./src/utils/common";
 import { HomePage } from "./src/pages/HomePage";
 import { AboutPage } from "./src/pages/AboutPage";
 import SignUpPage from "./src/pages/SignUpPage";
 import NotFoundPage from "./src/pages/NotFoundPage";
+import validationForm from "./src/validations/auth.valid";
 
 const app = document.getElementById("app");
 const router = new Navigo("/", { linksSelector: "a" });
 router.on("/home", () => render(app, HomePage));
 router.on("/about", () => render(app, AboutPage));
-router.on("/signup", () => render(app, SignUpPage));
+router.on("/signup", () => render(app, SignUpPage), {
+  after() {
+    const register = () => {
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      var confirmPass = document.getElementById("confirmPass").value;
+
+      // Buoc 2: Gom du lieu vao object
+      var userInfor = {
+        email,
+        password,
+        confirmPass,
+      };
+
+      if (validationForm(userInfor)) {
+        users.push(userInfor);
+        // Buoc 3: Đưa vào localStorage.
+        // localStorage.setItem("vidu", users);
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("Dang ky thanh cong!");
+      }
+    };
+
+    const registerBtn = document.getElementById("registerBtn");
+    registerBtn.onclick = register;
+  },
+});
+router.on("/", () => router.navigate("/home"));
 router.notFound(() => render(app, NotFoundPage));
 router.resolve();
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,31 @@
 const SignUpPage = () => {
   return `
-  <h1>SignUpPage<h1>
+  <form action="">
+      <h1>Register</h1>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" />
+      </div>
+
+      <div class="mb-3">
+        <label for="confirmPass" class="form-label">Confirm password</label>
+        <input type="password" class="form-control" id="confirmPass" />
+      </div>
+
+      <button type="button" id="registerBtn" onclick="register()" class="btn btn-primary w-100">
+        Register
+      </button>
+    </form>
+
   `;
 };
 
