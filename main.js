a53e22d9b200742458d02cd766d084fe3b7ec374
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-15 16:29:21 +0700

    Buổi 6: add register form

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ import Navigo from "navigo";
 import HomePage from "./src/pages/HomePage.js";
 import { render } from "./src/utils/common.js";
 import NotFoundPage from "./src/pages/NotFoundPage.js";
+import RegisterPage from "./src/pages/RegisterPage.js";
+import LoginPage from "./src/pages/LoginPage.js";
 
 const router = new Navigo("/", { linksSelector: "a" });
 const app = document.getElementById("app");
@@ -13,5 +15,7 @@ const app = document.getElementById("app");
 router.on("/", () => render(app, HomePage));
 router.on("/home", () => router.navigate("/"));
 router.on("/about", () => render(app, AboutPage));
+router.on("/signup", () => render(app, RegisterPage));
+router.on("/signin", () => render(app, LoginPage));
 router.notFound(() => render(app, NotFoundPage));
 router.resolve();
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,5 +1,81 @@
 const RegisterPage = () => {
-  return `ComponentContent`;
+  var users = [];
+  function validationForm(username, password, confirmPass) {
+    // Kiem tra du lieu
+    if (username === "" || password === "" || confirmPass === "") {
+      alert("khong duoc de trong!");
+      return false;
+    }
+    if (password.length < 6) {
+      alert("Password phai dai it nhat 6 ky tu!");
+      return false;
+    }
+    if (password !== confirmPass) {
+      alert("Confirm password không khớp!");
+      return false;
+    }
+    return true;
+  }
+  function register() {
+    // Buoc 1: lay gia tri
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var confirmPass = document.getElementById("confirmPass").value;
+
+    if (validationForm(username, password, confirmPass)) {
+      // Buoc 2: Dua vao 1 object
+      var userInfor = {
+        username,
+        password,
+        confirmPass,
+      };
+
+      users.push(userInfor);
+      // Buoc 3: dua userInfor vao localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+      alert("Dang ky thanh cong!");
+    }
+  }
+  // Xây dựng trang đăng nhập, nếu khớp dữ liệu trong localStorage thì báo đăng nhập thành công!
+
+  return `
+  <form action="">
+  <h1>Register</h1>
+  <div class="mb-3">
+    <label for="username" class="form-label">Username:</label>
+    <input
+      type="text"
+      class="form-control"
+      id="username"
+      placeholder="Your username..."
+    />
+  </div>
+
+  <div class="mb-3">
+    <label for="password" class="form-label">Password:</label>
+    <input
+      type="password"
+      class="form-control"
+      id="password"
+      placeholder="Your password..."
+    />
+  </div>
+  <div class="mb-3">
+    <label for="confirmPass" class="form-label">ConfirmPass:</label>
+    <input
+      type="password"
+      class="form-control"
+      id="confirmPass"
+      placeholder="Your confirmPass..."
+    />
+  </div>
+  <div class="mb-3">
+    <button class="btn btn-primary" type="button" onclick="register()">
+      Register
+    </button>
+  </div>
+</form>
+  `;
 };
 
 export default RegisterPage;
