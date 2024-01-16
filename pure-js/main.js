5d1f86e7d6f9d1a8d350d7f6e6cbe279575b07b8
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-16 20:31:50 +0700

    Buổi 7: fetch API

--- pure-js/main.js
+++ pure-js/main.js
@@ -1,26 +1,19 @@
-const products = [];
-
-fetch("http://localhost:3000/products")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    products.push(...data);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log("done");
-  });
-
 const app = document.getElementById("app");
-const htmlContent = products
-  .map((item) => {
-    return `
-  <h1>${item.title}</h1>
-  `;
-  })
-  .join("");
 
-console.log(htmlContent);
+const callAPI = async () => {
+  const response = await fetch("http://localhost:3000/products");
+  const data = await response.json();
+  console.log(data);
+
+  const htmlContent = data
+    .map((item) => {
+      return `
+      <h1>${item.title}</h1>
+  `;
+    })
+    .join("");
+
+  app.innerHTML = htmlContent;
+};
+
+callAPI();
